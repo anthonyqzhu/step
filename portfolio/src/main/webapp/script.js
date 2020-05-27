75fc8ea1474e071c96caf651e62355c8e56aa0d1
Anthony Zhu <aqzhu@umich.edu>
2020-05-27 16:05:42 -0400

    Add function to add a random tv quote to the page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,6 +16,7 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
+
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
@@ -25,4 +26,20 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+/** 
+ * Adds a random quote to the page.
+ */
+function addRandomQuote() {
+
+  const tv_quotes =
+      ['"Winter is coming."', '"Most men would rather deny a hard truth than face it."', '"What do we say to the Lord of Death?"\n"Not today."']
+
+  // Pick a random TV quote
+  const quote = tv_quotes[Math.floor(Math.random() * tv_quotes.length)];
+
+  // Add it to the page.
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
 }
